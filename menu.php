837fcce43aecd814222309979cfa5c775204c261
Evandro <evandromachadopereira@hotmail.com>
2019-10-16 16:14:54 -0300

    Update  1.3
    Tela de menu

--- menu.php
+++ menu.php
@@ -19,8 +19,25 @@
 <div class="id-bar-user">
     <p>@Usuario <a href="#">Logout</a></p>
 </div>
-
-
+<main>
+    <h2>Fichas</h2>
+    <ul class="list-group">
+        <!-- inicio  do loop -->
+        <form action="#">
+            <button type="submit" name="#id_da_ficha" class="btn btn-lg btn-block">
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col">Personagem</div>
+                        <div class="col">Raça</div>
+                        <div class="col">Classe</div>
+                        <div class="col">Nivel</div>
+                    </div>
+                </li>
+            </button>
+        </form>
+        <!-- fim do loop -->
+    </ul>
+</main>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
