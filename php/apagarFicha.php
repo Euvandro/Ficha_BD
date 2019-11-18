0a433ee54d4af545adcf788b25d54da809d2ca93
Evandro <evandromachadopereira@hotmail.com>
2019-11-18 13:36:28 -0300

    Update 2.0.1
    Delete ficha incompleto

---
+++ php/apagarFicha.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Evandro
+ * Date: 18/11/2019
+ * Time: 14:35
+ */
--- visualizaFicha.php
+++ visualizaFicha.php
@@ -496,6 +496,9 @@ $linhaArmadura = mysqli_fetch_assoc($resultadoArmadura);
 
 <br>
 <button type="submit" name="id_ficha" class="btn btn-outline-success btn-lg p-3" value="<?= $id_ficha ?>">Salvar Ficha</button>
+        <form action="php/apagarFicha.php" method="post">
+            <button type="submit" name="id_ficha" class="btn btn-outline-danger btn-lg p-3" value="<?= $id_ficha ?>">Apagar Ficha</button>
+        </form>
 </form>
 </main>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
